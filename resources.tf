4b74dc5a5cd69fff47b57b20504c09addb4d3335
stemsrd <intrepid110915@gmail.com>
2022-06-09 14:52:46 +0200

    commit

--- resources.tf
+++ resources.tf
@@ -37,6 +37,13 @@ resource "aws_security_group" "unbound_security_group" {
         cidr_blocks = ["0.0.0.0/0"]
       }
 
+      egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+      }
+
       tags= {
         Name = "security_unbound"
       }
